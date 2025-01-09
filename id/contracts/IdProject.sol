b7982550fcf9b3898a6cac15ec0524cc41e533d1
jsunnypi <appleorange0314@gmail.com>
2025-01-09 11:36:39 +0900

    250109

--- id/contracts/IdProject.sol
+++ id/contracts/IdProject.sol
@@ -1,33 +1,57 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract IdProject{
-    
-    struct Game{
-        uint id;
-        string gameName;
-        string serverName;
-        string className;      
-        string remarks;
+contract IdProject {
+    struct Game {
+        uint[] blockId;
+        mapping(uint256 => string) gameName;
+        mapping(uint256 => string) serverName;
+        mapping(uint256 => string) className;
+        mapping(uint256 => string) remarks;
     }
 
-    mapping(address => Game) public games;
-   
-    uint public blockCount;
+    mapping(address => Game) private games;
 
-    function insertInfo(string memory _gameName, string memory _serverName, string memory _className, string memory _remarks) public {
-       
+    uint256 public blockCount;
+
+    function insertInfo(
+        string memory _gameName,
+        string memory _serverName,
+        string memory _className,
+        string memory _remarks
+    ) public {
         require(bytes(_gameName).length > 0, "Game Name is required");
         require(bytes(_serverName).length > 0, "Server Name is required");
         require(bytes(_className).length > 0, "Class Name is required");
-
         blockCount++;
-        games[msg.sender] = Game(blockCount, _gameName, _serverName, _className, _remarks);
-       
+        games[msg.sender].blockId.push(blockCount);
+        games[msg.sender].gameName[blockCount] = _gameName;
+        games[msg.sender].serverName[blockCount] = _serverName;
+        games[msg.sender].className[blockCount] = _className;
+        games[msg.sender].remarks[blockCount] = _remarks;
     }
 
-    function getInfo() public view returns(uint, string memory, string memory, string memory, string memory){
-        return (games[msg.sender].id, games[msg.sender].gameName, games[msg.sender].serverName, games[msg.sender].className, games[msg.sender].remarks);
+    event Test(uint);
+
+    function getIndexOf(address _address) public view returns(uint[] memory){
+        return games[_address].blockId;
     }
 
+    function getInfo(address _address, uint _blockId)
+        public
+        view
+        returns (
+            string memory,
+            string memory,
+            string memory,
+            string memory
+        )
+    {
+        return (
+            games[_address].gameName[_blockId],
+            games[_address].serverName[_blockId],
+            games[_address].className[_blockId],
+            games[_address].remarks[_blockId]
+        );
+    }
 }
